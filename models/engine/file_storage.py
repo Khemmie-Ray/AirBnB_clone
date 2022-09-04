cc53bf9bd531632450ca7042c62d68a19b287da0
Khemmie-Ray <atokemmy@gmail.com>
2022-09-04 22:43:04 +0100

    project

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,11 @@ from models.review import Review
 
 
 class FileStorage:
-    """Construct"""
+    """Represent an abstracted storage engine.
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
     __file_path = "file.json"
     __objects = {}
 
